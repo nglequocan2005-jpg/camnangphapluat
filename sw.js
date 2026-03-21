22ed82fed7c56d62aa0d520e2a55c946e081642c
nglequocan2005-jpg <nglequocan2005@gmail.com>
2026-03-21 20:50:47 +0700

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', (e) => {
-  console.log('Service Worker: Đã cài đặt thành công');
-});
-
-self.addEventListener('fetch', (e) => {
-  // Bắt buộc phải có dòng này để nút "Tải App" xuất hiện
-});
